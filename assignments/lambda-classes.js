8f6f858130e13fad38545a7101a8942293c37a43
WilliamBerlin76 <william.berlin76@gmail.com>
2019-08-22 14:48:33 -0500

    Instructor and Person class created

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,27 @@
 // CODE here for your Lambda Classes
+class Person{
+    constructor(attributes){
+        this.name = attributes.name,
+        this.age = attributes.age,
+        this.location = attributes.location
+    };
+    speak(){
+        return `Hello my name is ${this.name}, I am from ${this.location}`;
+    };
+};
+
+class Instructor extends Person{
+    constructor(attributes){
+        super(attributes);
+        this.specialty = attributes.specialty,
+        this.favLanguage = attributes.favLanguage,
+        this.catchPhrase = attributes.catchPhrase
+    };
+    demo(subject){
+        return `today we are learning about ${subject}`
+    };
+    grade(student, subject){
+        return `${student.name} recieves a perfect score on ${subject}`
+    }
+};
+
