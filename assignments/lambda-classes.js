b1d95c709317631867d219e7617af1b93e946a9c
WilliamBerlin76 <william.berlin76@gmail.com>
2019-08-22 15:31:22 -0500

    classes added

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -4,7 +4,7 @@ class Person{
         this.name = attributes.name,
         this.age = attributes.age,
         this.location = attributes.location
-    };
+    }
     speak(){
         return `Hello my name is ${this.name}, I am from ${this.location}`;
     };
@@ -16,12 +16,46 @@ class Instructor extends Person{
         this.specialty = attributes.specialty,
         this.favLanguage = attributes.favLanguage,
         this.catchPhrase = attributes.catchPhrase
-    };
+    }
     demo(subject){
-        return `today we are learning about ${subject}`
-    };
-    grade(student, subject){
-        return `${student.name} recieves a perfect score on ${subject}`
+        return `today we are learning about ${subject}`;
+    }
+    grade(Student, subject){
+        return `${Student.name} recieves a perfect score on ${subject}`;
     }
 };
+
+class Student extends Person{
+    constructor(attributes){
+        super(attributes);
+        this.previousBackground = attributes.previousBackground,
+        this.className = attributes.className,
+        this.favSubjects = attributes.favSubjects
+    }
+    listsSubjects(){
+        this.favSubjects.forEach(function(item){
+            console.log(item)
+        })
+    }
+    PRAssignment(subject){
+        return `${Student.name} has submitted a PR for ${subject}`
+    }
+    sprintChallenge(subject){
+        return `${Student.name} had begun sprint challenge on ${subject}`
+    }
+}
+
+class ProjectManager extends Instructor{
+    constructor(attributes){
+        super(attributes);
+        this.gradClassName = attributes.gradClassName,
+        this.favInstructor = attributes.favInstructor
+    }
+    standUp(channel){
+        return `${this.name} announces to ${channel}, @channel standy times!`
+    }
+    debugsCode(Student, subject){
+        return `${this.name} debugs ${Student.name}'s code on ${subject}`
+    }
+}
 
